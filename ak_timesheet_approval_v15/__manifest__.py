b5d9ae89741c560fafe673fc584772c5490ec4ba
Vipin <praveen.odoo@gmail.com>
2023-09-04 17:55:26 +0530

    change the title

--- ak_timesheet_approval_v15/__manifest__.py
+++ ak_timesheet_approval_v15/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Time sheet Validation",
+    'name': "Timesheet Validation",
 
     'summary': """
         Manager can approve and reject the time sheet of an employee.""",
